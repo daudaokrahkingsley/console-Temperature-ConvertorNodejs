4a017dca8c569ea4052fb8a198b00ccf2eba27c3
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2022-12-20 13:14:56 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function celsiusTofahrenherit(celsius){
+    let fahrenherit=(celsius* 1.8)+32;
+    console.log(celsius+ " cencius is equal to " + fahrenherit + " Ferenheigt");
+}
+
+
+function fahrenheritToCelsius(fahrenherit){
+    let cencius=(fahrenherit-32)*0.556;
+    console.log(fahrenherit + " fahrenherit is equal to " + fahrenherit + "celsius");
+
+
+}
+
+celsiusTofahrenherit(50);
+
+fahrenheritToCelsius(50);
